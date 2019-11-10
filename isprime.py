6ad5f9050b6153b7021d164157dbba21dc074cff
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2019-11-10 12:05:42 +0200

    Add files via upload

---
+++ isprime.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+def is_prime(x):
+    if x < 2:
+        return False
+    else:
+        for n in range(2, x - 1):
+            if x % n == 0:
+                return False
+        return True
+
+
+x = randint(0, 1000)
+print(x, is_prime(x))
