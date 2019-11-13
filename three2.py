d3832dfbe2b83701645e45f55f1dcac6e50f83ef
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2019-11-13 19:00:40 +0200

    Update three2.py

--- three2.py
+++ three2.py
@@ -5,5 +5,5 @@ print(lst)
 a = int(input('Введите a: '))
 b = int(input('Введите b: '))-2
 for i in range(a-1, int((a-1+b)+1/2)//2):
-    lst[i], lst[b-(i-a)] = lst[b-(i-a)], lst[i]
+    lst[i], lst[b-(i-a+1)] = lst[b-(i-a+1)], lst[i]
 print(lst)
